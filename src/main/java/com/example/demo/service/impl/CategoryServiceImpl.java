dd1814369bb6e916b74f74a27e38b87ec98f707f
bala-amypo <bala@amypo.in>
2025-12-21 13:54:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Autowired
+
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
