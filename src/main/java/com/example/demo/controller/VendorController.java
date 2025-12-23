b5e90907911121742eda429ef0d412b70e201a34
bala-amypo <bala@amypo.in>
2025-12-23 04:17:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/VendorController.java
+++ src/main/java/com/example/demo/controller/VendorController.java
@@ -6,6 +6,7 @@ import com.example.demo.service.VendorService;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/vendors")
 public class VendorController {
