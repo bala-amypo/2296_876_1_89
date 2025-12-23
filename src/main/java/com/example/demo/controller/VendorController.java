70f5e81d9758bf57c524518ea49bba796fbb7b17
bala-amypo <bala@amypo.in>
2025-12-23 05:32:37 +0000

    User commit

--- src/main/java/com/example/demo/controller/VendorController.java
+++ src/main/java/com/example/demo/controller/VendorController.java
@@ -1,28 +1,29 @@
- 
 package com.example.demo.controller;
 
-import com.example.demo.entity.Vendor;
+import com.example.demo.dto.VendorDto;
 import com.example.demo.service.VendorService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-@CrossOrigin(origins = "*")
+
 @RestController
 @RequestMapping("/api/vendors")
 public class VendorController {
 
-    private final VendorService service;
+    private final VendorService vendorService;
 
-    public VendorController(VendorService service) {
-        this.service = service;
+    public VendorController(VendorService vendorService) {
+        this.vendorService = vendorService;
     }
 
     @PostMapping
-    public Vendor create(@RequestBody Vendor vendor) {
-        return service.createVendor(vendor);
+    public ResponseEntity<VendorDto> createVendor(@RequestBody VendorDto vendorDto) {
+        return ResponseEntity.ok(vendorService.createVendor(vendorDto));
     }
 
     @GetMapping
-    public List<Vendor> list() {
-        return service.getAllVendors();
+    public ResponseEntity<List<VendorDto>> getAllVendors() {
+        return ResponseEntity.ok(vendorService.getAllVendors());
     }
 }
