fac79a486909366946545ae43319caa1083703de
bala-amypo <bala@amypo.in>
2025-12-26 10:21:57 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -3,10 +3,13 @@ package com.example.demo.service.impl;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Category;
 import com.example.demo.repository.CategoryRepository;
+import com.example.demo.service.CategoryService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class CategoryServiceImpl {
+@Service   // ⭐ REQUIRED
+public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
 
@@ -14,15 +17,18 @@ public class CategoryServiceImpl {
         this.categoryRepository = categoryRepository;
     }
 
+    @Override
     public Category createCategory(Category category) {
         return categoryRepository.save(category);
     }
 
+    @Override
     public Category getCategory(Long id) {
         return categoryRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
     }
 
+    @Override
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
