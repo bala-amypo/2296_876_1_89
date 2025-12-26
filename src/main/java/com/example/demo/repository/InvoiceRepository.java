ce7c4a078a62198c46b23ae881fff55a4a3b5c63
bala-amypo <bala@amypo.in>
2025-12-26 10:45:56 +0000

    User commit

--- src/main/java/com/example/demo/repository/InvoiceRepository.java
+++ src/main/java/com/example/demo/repository/InvoiceRepository.java
@@ -3,13 +3,15 @@ package com.example.demo.repository;
 import com.example.demo.model.Invoice;
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
     List<Invoice> findByUploadedBy(User user);
 
-    // Used directly in test cases (mocked)
-    List<Invoice> findByAmountGreaterThanHql(Double amount);
+    // ✅ FIXED METHOD
+    List<Invoice> findByAmountGreaterThan(Double amount);
 }
